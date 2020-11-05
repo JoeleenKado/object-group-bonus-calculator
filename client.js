d9dbc8ef20ddee341b6a14c1a2a1b9db5d3e96bb
Joelle Kado <jkado@my.hpu.edu>
2020-11-04 20:47:59 -0600

    Assignment Submission

--- client.js
+++ client.js
@@ -28,7 +28,14 @@ const employees = [
     employeeNumber: '89068',
     annualSalary: '35000',
     reviewRating: 1
+  },
+  {
+    name: 'Fred',
+    employeeNumber: '8906',
+    annualSalary: '35000',
+    reviewRating: 1
   }
+  
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -41,3 +48,56 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+for (let employeeItem of employees){
+  let results = processEmployee(employeeItem);
+  console.log('Processed employee is:', results);
+}
+
+function processEmployee (employee) {
+  console.log('Employee is', employee);
+  let bonusPercentage = calculateBonusPercentage(employee);
+  let totalBonus = employee.annualSalary * bonusPercentage *.01;
+  let newEmployeeObject = {
+    
+    name: employee.name,
+    totalCompensation : Number(employee.annualSalary) + totalBonus,
+    bonusPercentage: bonusPercentage,
+    totalBonus: totalBonus
+  }
+  return newEmployeeObject
+}
+
+function calculateBonusPercentage(employee){
+  let bonusPercentage = 0;
+  switch (employee.reviewRating) {
+    case 0:
+    case 1:
+    case 2: 
+    bonusPercentage = 0;
+    break;
+    case 3: 
+    bonusPercentage = 4;
+    break;
+    case 4:
+      bonusPercentage =6;
+      break; 
+      case 5:
+       bonusPercentage = 10;
+       break;
+  }
+  
+  if (employee.reviewRating > 2 && employee.employeeNumber.length === 4) {
+    bonusPercentage += 5;
+  }
+  if(employee.annualSalary > 65000){
+    bonusPercentage -= 1;
+  }
+  
+  if (bonusPercentage < 0) {
+      bonusPercentage = 0;
+  } else if (bonusPercentage > 13) {
+    bonusPercentage = 13;
+  }
+  return bonusPercentage;
+}
